94e8d08811ba305c5c1d439f2bce38d1063e5df6
Pietro Falaschi <pitpiter@MacBook-Pro-di-Pietro-2.local>
2023-05-08 19:47:02 +0200

    fatta l'assegnazione delle variabili della struttura t_data

--- inc/philosopher.h
+++ inc/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:19:51 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/05/04 19:30:36 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:34:04 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,39 @@
 # include <stdio.h>
 # include <pthread.h>
 # include <sys/time.h>
+
+struct s_data;
+
+typedef struct s_philo
+{
+	struct			s_data *data;
+	pthread_t		t1;
+	int 			id;
+	int				eat_count;
+	int 			status;
+	int		 		eating;
+	uint64_t 		time_to_die;
+	pthread_mutex_t lock;
+	pthread_mutex_t r_fork;
+	pthread_mutex_t l_fork;
+}		t_philo;
+
+typedef struct	s_data
+{
+	pthread_t		*tid;
+	int				philo_num;
+	int				meals_nb;
+	int				dead;
+	int 			stop;
+	t_philo 		*philos;
+	u_int64_t		death_time;
+	u_int64_t		eat_time;
+	u_int64_t		sleep_time;
+	u_int64_t		start_time;
+	pthread_mutex_t *forks;
+	pthread_mutex_t lock;
+	pthread_mutex_t write;
+} 		t_data;
 
 //	input_err
 # define ERR_IN_1 "INVALID INPUT CHARACTER"
--- src/main.c
+++ src/main.c
@@ -6,12 +6,62 @@
 /*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 19:35:41 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/05/04 20:34:34 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:46:08 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "../inc/philosopher.h"
 
+/* per tradurre gli argomenti da testo a digit utilizziamo atoi
+ci passiamo tutti gli argomenti nella struttura data
+siccome possono essere sia 5 che 6 gli argomenti, dobbiamo gestire il numero
+di pasti se specificati
+se ac == 5, allora il meal_nb lo metto in negativo in modo che non nel
+conteggio che verrà fatto non inficierà sulle funzioni successive e non
+si fermerà
+controlliamo poi che gli argomenti inseriti siano validi per far funzionare
+il programma
+la variabile dead (e finished) è inizializata a 0 perchè ci servirà più 
+avanti per fermare il programma in caso di una morte
+le ultime due chiamate a funzioni sono per inizializzare write e lock, anche queste
+variabili all'interno della struttura.
+La variabile "write" è utilizzata per garantire che la scrittura su stdout 
+sia eseguita in modo sincronizzato dai vari thread, 
+mentre la variabile "lock" è utilizzata per proteggere l'accesso a risorse 
+condivise. */
+
+int	ft_init_data(t_data *data, int ac, int **av)
+{
+	data->philo_num = (int)ft_atoi(av[1]);
+	data->death_time = (u_int64_t)ft_atoi(av[2]);
+	data->eat_time = (u_int64_t)ft_atoi(av[3]);
+	data->sleep_time = (u_int64_t)ft_atoi(av[4]);
+	if (ac == 6)
+		data->meals_nb = (int)ft_atoi(av[5]);
+	else
+		data->meals_nb = -1;
+	if (data->philo_num <= 0 || data->philo_num >= 200 || data->death_time < 0
+		|| data->eat_time < 0 || data->sleep_time < 0)
+	{
+		printf("uno degli argomento ha un valore errato\n");
+		printf("prendi spunto dal seguente: 5 800 200 200 7\n");
+		return (1);
+	}
+	data->dead = 0;
+	data->stop = 0;
+	pthread_mutex_init(&data->write, NULL);
+	pthread_mutex_init(&data->lock, NULL);
+}
+/* 	questa funzione mi serve per inizializzare tutti i valori delle struct e per
+	eseguire i check del caso per gli input */
+
+	int ft_init(t_data *data, int **ac, int av)
+{
+	if (ft_init_data(data, av, ac))
+		return (1);
+}
+
+// Controllo subito con questa funzione che i caratteri inseriti siano giusti
 int	input_checker(char **av)
 {
 	int	i;
@@ -49,5 +99,6 @@ int	main(int ac, char **av)
 		printf("controlla gli input, qualcosa non torna!");
 		return (1);
 	}
-	
+	if (ft_init(&data, av, ac))
+		return (1);
 }
